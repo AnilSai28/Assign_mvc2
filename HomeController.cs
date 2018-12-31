61b5a2ca241a9c27bf52181c5c9fc09bd6922181
AnilSai28 <46150420+AnilSai28@users.noreply.github.com>
2018-12-31 15:27:14 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_day1.Controllers
+{
+    public class HomeController : Controller
+    {
+      public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(string Loginid, string Password)
+        {
+            if (Loginid == "user1" && Password == "pass@123")
+            {
+                Session["loginid"] = Loginid;
+                return RedirectToAction("index", "home"); //get
+            }
+            else
+            {
+                ViewBag.msg = "invalid id or password";
+                return View();
+            }
+        }
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult NewUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewUser
+            (string emailid,string password,string name,string city)
+        {
+            return View("view_customercreated");
+        }
+        public ActionResult logout()
+        {
+            Session.Clear();
+            return RedirectToAction("login", "home");
+        }
+
+    }
+}
